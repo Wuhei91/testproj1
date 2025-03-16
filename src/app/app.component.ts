6c6e4fc509e5f00dc44851006d1df36306ffed30
cgang91 <pek1991@hotmail.com>
2025-03-16 20:43:35 +0800

    init wed invitation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,25 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [FormsModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'testproj1';
+  messages = [
+    { name: 'John', comment: 'Excited for the wedding!' },
+    { name: 'Jane', comment: 'Can’t wait to celebrate with you!' }
+  ];
+  newMessage = { name: '', comment: '' };
+
+  addMessage() {
+    if (this.newMessage.name && this.newMessage.comment) {
+      this.messages.push({ ...this.newMessage });
+      this.newMessage = { name: '', comment: '' };
+    }
+  }
 }
